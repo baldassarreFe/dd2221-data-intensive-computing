9aba6555fa33ab2d7679cfc2121e13f354aea2cc
Federico Baldassarre <baldassarre.fe@gmail.com>
2017-10-06 13:54:59 +0200

    Setup lab 3

---
+++ 3 - Flink/flink-training-exercises/src/main/java/com/dataartisans/flinktraining/exercises/datastream_java/datatypes/EnrichedTrade.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2017 data Artisans GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dataartisans.flinktraining.exercises.datastream_java.datatypes;
+
+public class EnrichedTrade implements Comparable<EnrichedTrade> {
+
+	public EnrichedTrade() {}
+
+	public EnrichedTrade(Trade trade, String customerInfo) {
+
+		this.trade = trade;
+		this.customerInfo = customerInfo;
+	}
+
+	public Trade trade;
+	public String customerInfo;
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("EnrichedTrade(").append(trade.timestamp).append(") ");
+		sb.append(customerInfo);
+		return sb.toString();
+	}
+
+	public int compareTo(EnrichedTrade other) {
+		return Long.compare(this.trade.timestamp, other.trade.timestamp);
+	}
+}
